c05ee96e77498088ce5782662aa0a3c4cc3c55ba
Maxime BJ <maxime@dysign.fr>
2025-06-05 12:06:50 +0200

    Added filter to display only caught pokemon

--- hooks/register-rest-routes.php
+++ hooks/register-rest-routes.php
@@ -6,65 +6,34 @@ class RegisterRestRoutes
 {
   public function register_hooks()
   {
-    add_action('rest_api_init', [$this, 'register_routes']);
+    add_filter('rest_pokemon_collection_params', [$this, 'add_type_param']);
+    add_filter('rest_pokemon_query', [$this, 'add_type_filter'], 10, 2);
   }
 
-  public function register_routes()
+  public function add_type_param($params)
   {
-    register_rest_route('proximit/v1', '/pokemon', [
-      'methods' => 'GET',
-      'callback' => [$this, 'get_pokemon'],
-      'permission_callback' => '__return_true',
-      'args' => [
-        'type' => [
-          'required' => false,
-          'type' => 'string',
-          'sanitize_callback' => 'sanitize_text_field',
-        ],
-      ],
-    ]);
-  }
-
-  public function get_pokemon($request)
-  {
-    $type = $request->get_param('type');
-
-    $args = [
-      'post_type' => PROXIMIT_IMPORT_POST_TYPE,
-      'posts_per_page' => -1,
-      'post_status' => 'publish',
+    $params['caught'] = [
+      'description' => 'Display only caught pokemons',
+      'type' => 'boolean',
+      'required' => false,
     ];
 
-    if ($type) {
-      $args['tax_query'] = [
+    return $params;
+  }
+
+  public function add_type_filter($args, $request)
+  {
+    $caught = $request->get_param('caught');
+
+    if ($caught) {
+      $args['meta_query'] = [
         [
-          'taxonomy' => 'pokemon_type',
-          'field' => 'slug',
-          'terms' => $type,
+          'key' => 'caught',
+          'value' => '1',
         ],
       ];
     }
 
-    $query = new \WP_Query($args);
-    $pokemon = [];
-
-    if ($query->have_posts()) {
-      while ($query->have_posts()) {
-        $query->the_post();
-        $post_id = get_the_ID();
-
-        $pokemon[] = [
-          'id' => get_post_meta($post_id, 'pokemon_id', true),
-          'name' => get_the_title(),
-          'content' => get_the_content(),
-          'thumbnail' => get_the_post_thumbnail_url($post_id, 'full'),
-          'types' => wp_get_post_terms($post_id, 'pokemon_type', ['fields' => 'names']),
-        ];
-      }
-    }
-
-    wp_reset_postdata();
-
-    return rest_ensure_response($pokemon);
+    return $args;
   }
 }
